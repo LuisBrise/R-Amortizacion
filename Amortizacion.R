fee40df6a805358631d7bcbd6c7d677331ad977f
LuisBrise <70853052+LuisBrise@users.noreply.github.com>
2022-10-14 07:45:14 -0500

    Add files via upload

---
+++ Amortizacion.R
@@ -0,0 +1,11 @@
+# Prueba Unilogic R
+
+# Parte 1
+# Leer los archivos Creditos y Calendarios
+library(readxl) # Leer archivos de Excel
+Calendarios <- read_excel("Datos/Calendarios.xlsx")
+Creditos <- read_excel("Datos/Creditos.xlsx")
+
+# Puedes usar "attributes" para conocer las entradas de los data.frames
+
+https://www.youtube.com/watch?v=FIOV4g94bZw
