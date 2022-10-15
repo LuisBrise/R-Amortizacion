ef855d918e4a3ba28553b727207b1d74cf09dc00
Jorge Luis Briseño Gómez <jorgeluisbrisenio@ciencias.unam.mx>
2022-10-15 13:44:14 -0500

    Commit from RStudio

--- Amortizacion.R
+++ Amortizacion.R
@@ -1,11 +1,42 @@
 # Prueba Unilogic R
 
 # Parte 1
-# Leer los archivos Creditos y Calendarios
+
+#Bibliotecas
 library(readxl) # Leer archivos de Excel
-Calendarios <- read_excel("Datos/Calendarios.xlsx")
-Creditos <- read_excel("Datos/Creditos.xlsx")
+library(plyr) # Tools for Splitting, Applying and Combining Data
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+
+# Leer los archivos Creditos y Calendarios
+Calendarios <- read_excel("Calendarios.xlsx")
+Creditos <- read_excel("Creditos.xlsx")
 
 # Puedes usar "attributes" para conocer las entradas de los data.frames
 
-https://www.youtube.com/watch?v=FIOV4g94bZw
+levels(Creditos$periodicidadamortizacion)
+periodos <- levels(as.factor(Creditos$periodicidadamortizacion))
+esquemas.amort <- levels(as.factor(Creditos$esquemadeamortizacion))
+
+options(digits = 7) # precisión 7
+options(scipen = 999) #sin notación científica
+
+saldoInsoluto <<- numeric() 
+interes <- numeric()
+amortizacion <<- numeric()
+amortizacionAcumulada <<- numeric()
+
+amorizacionNormal <- funcion(idcredito,
+                             capital, 
+                             interes, 
+                             periodos, 
+                             fechainicial, 
+                             fechafinal,
+                             mensual=TRUE){
+  if(mensual){
+    tasaDEinteres <- interes/12
+  }
+}
+
+Calendarios[Calendarios$idcredito==1,]
