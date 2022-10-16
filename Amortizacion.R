d457e2979f5b75791e665eda13115816d7b2a776
Jorge Luis Briseño Gómez <jorgeluisbrisenio@ciencias.unam.mx>
2022-10-16 09:57:10 -0500

    Commit from RStudio

--- Amortizacion.R
+++ Amortizacion.R
@@ -10,12 +10,11 @@ library(ggplot2)
 library(tidyverse)
 
 # Leer los archivos Creditos y Calendarios
-Calendarios <- read_excel("Calendarios.xlsx")
-Creditos <- read_excel("Creditos.xlsx")
+Calendarios.df <- read_excel("Calendarios.xlsx")
+Creditos.df <- read_excel("Creditos.xlsx")
 
 # Puedes usar "attributes" para conocer las entradas de los data.frames
 
-levels(Creditos$periodicidadamortizacion)
 periodos <- levels(as.factor(Creditos$periodicidadamortizacion))
 esquemas.amort <- levels(as.factor(Creditos$esquemadeamortizacion))
 
@@ -27,13 +26,20 @@ interes <- numeric()
 amortizacion <<- numeric()
 amortizacionAcumulada <<- numeric()
 
-amorizacionNormal <- funcion(idcredito,
-                             capital, 
-                             interes, 
-                             periodos, 
-                             fechainicial, 
-                             fechafinal,
-                             mensual=TRUE){
+amortizacionNormal1 <- function(df){
+  
+  id = df$idcredito
+  periodicidad = df$periodicidadamortizacion
+  esquema = df$esquemadeamortizacion
+  iva = df$tasaiva
+  tasa.interes = df$tasacontractual
+  capital = df$saldoinicialcredito
+  
+  fechas.df = Calendarios.df[Calendarios$idcredito==id,] # Selecciona solo las fechas con el mismo idcredito
+  periodos = length(fechas.df$fechainicialmes) #Cuanto el número de fechas
+  periodo.primermes = as.numeric(fechas.df$fechafinalmes[1] 
+                                 - fechas.df$fechainicialmes[1])
+  
   if(mensual){
     tasaDEinteres <- interes/12
   }
